59a52837c99e8da425c6fcd35f371a611d048d98
AjayTumula <ajaytumula72@gmail.com>
2024-08-18 11:41:30 +0530

    fixed the config

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-import Image from "next/image";
+
 import Mnemonic from "./create-mnemonic";
 import { SolanaWallet } from "./components/solana-wallet";
 import { EthWallet } from "./components/ethereum-wallet";
 
 export default function Home() {
   return (
-   <main>
+   <main className="place-items-center p-20">
       <Mnemonic /> 
       <SolanaWallet />
       <EthWallet />
